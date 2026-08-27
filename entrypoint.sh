ba18b513e48bcf8f4508c15d8fec92f2218788c4
ftalijic <129983718+ftalijic@users.noreply.github.com>
2026-08-27 22:34:21 +0200

    Add entrypoint script for RealityScan execution

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+/opt/realityscan/bin/wine --start "$RS_EXE" -setInstanceName $CON_NAME $RS_ARGS &
+pid1=$!
+sleep 10
+/opt/realityscan/bin/wine --start "$RS_EXE" -delegateTo $CON_NAME $RSREMOTE_ARGS
+wait $pid1
